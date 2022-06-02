423cbfb9ebca4981acaa382ac15e347591f22a39
TripleKay <triplekay.k3@gmail.com>
2022-06-02 16:12:22 +0630

    ui added and get post data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+     <main>
+        <!-- Whats New Start -->
+        <router-view></router-view>
+        <!-- Whats New End -->
+
+        
+    </main>
 </template>
 
 <style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,189 @@
+<template>
+    
+    <section class="whats-news-area pt-50 pb-20">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row d-flex justify-content-between align-items-center">
+                        <div class="col-lg-3 col-md-3">
+                            <div class="section-tittle mb-30">
+                                <h3>Whats New</h3>
+                            </div>
+                        </div>
+                        <div class="col-lg-9 col-md-9">
+                            <div class="properties__button">
+                                <!--Nav Button  -->
+                                <nav>
+                                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="details.htmlnav-home" role="tab" aria-controls="nav-home" aria-selected="true">All</a
+                    >
+                    <a
+                        class="nav-item nav-link"
+                        id="nav-profile-tab"
+                        data-toggle="tab"
+                        href="details.htmlnav-profile"
+                        role="tab"
+                        aria-controls="nav-profile"
+                        aria-selected="false"
+                        >Lifestyle</a
+                    >
+                    <a
+                        class="nav-item nav-link"
+                        id="nav-contact-tab"
+                        data-toggle="tab"
+                        href="details.htmlnav-contact"
+                        role="tab"
+                        aria-controls="nav-contact"
+                        aria-selected="false"
+                        >Travel</a
+                    >
+                    <a
+                        class="nav-item nav-link"
+                        id="nav-last-tab"
+                        data-toggle="tab"
+                        href="details.htmlnav-last"
+                        role="tab"
+                        aria-controls="nav-contact"
+                        aria-selected="false"
+                        >Fashion</a
+                    >
+                    <a
+                        class="nav-item nav-link"
+                        id="nav-Sports"
+                        data-toggle="tab"
+                        href="details.htmlnav-nav-Sport"
+                        role="tab"
+                        aria-controls="nav-contact"
+                        aria-selected="false"
+                        >Sports</a
+                    >
+                    <a
+                        class="nav-item nav-link"
+                        id="nav-technology"
+                        data-toggle="tab"
+                        href="details.htmlnav-techno"
+                        role="tab"
+                        aria-controls="nav-contact"
+                        aria-selected="false"
+                        >Technology</a
+                    >
+                    </div>
+                                </nav>
+                                <!--End Nav Button  -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <div class="row">
+            <div class="col-12">
+                <!-- Nav Card -->
+                <div class="tab-content" id="nav-tabContent">
+                <!-- card one -->
+                <div
+                    class="tab-pane fade show active"
+                    id="nav-home"
+                    role="tabpanel"
+                    aria-labelledby="nav-home-tab"
+                >
+                    <div class="whats-news-caption">
+                    <div class="row">
+
+                        <div class="col-lg-6 col-md-6" v-for="(post , index) in postLists" :key="index">
+                            <div class="single-what-news mb-100">
+                                <div class="what-img">
+                                <img
+                                    v-bind:src="post.image"
+                                    alt=""
+                                />
+                                </div>
+                                <div class="what-cap">
+                                <span class="color1">Code Lab News</span>
+                                <h4>
+                                    <a href="details.html"
+                                    >{{ post.title }}</a
+                                    >
+                                </h4>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    </div>
+                </div>
+                
+                <!-- End Nav Card -->
+            </div>
+            </div>
+                </div>
+            </div>
+        </div>  
+             
+    </section>
+    <!--Start pagination -->
+        <div class="pagination-area pb-45 text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="single-wrap d-flex justify-content-center">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination justify-content-start">
+                                    <li class="page-item">
+                                        <a class="page-link" href="details.html"><span class="flaticon-arrow roted"></span
+                      ></a>
+                                            </li>
+                                            <li class="page-item active">
+                                                <a class="page-link" href="details.html">01</a>
+                                            </li>
+                                            <li class="page-item">
+                                                <a class="page-link" href="details.html">02</a>
+                                            </li>
+                                            <li class="page-item">
+                                                <a class="page-link" href="details.html">03</a>
+                                            </li>
+                                            <li class="page-item">
+                                                <a class="page-link" href="details.html"><span class="flaticon-arrow right-arrow"></span
+                      ></a>
+                                            </li>
+                                            </ul>
+                                    </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        </div>
+                    <!-- End pagination  -->
+    
+</template>
+
+<script>
+    import axios from "axios";
+    export default {
+        name: "HomePage",
+        data () {
+            return {
+                postLists: {},
+            }
+        },
+        methods: {
+            getAllPost () {
+                axios.get("http://127.0.0.1:8000/api/post").then((response) => {
+                    for(let i=0; i < response.data.post.length ; i++){
+                        if(response.data.post[i].image != null){
+                            response.data.post[i].image = "http://127.0.0.1:8000/postImage/"+response.data.post[i].image;
+                        }else{
+                            response.data.post[i].image = "http://127.0.0.1:8000/defaultImage/default_post.png";
+                        }
+                    }
+                    this.postLists = response.data.post;
+                })
+            }
+        },
+        mounted(){
+            this.getAllPost();
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
