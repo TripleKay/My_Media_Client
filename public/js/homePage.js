4ac8856ba21e14c8ae6e8c4f80b8d88d7747ffa0
TripleKay <triplekay.k3@gmail.com>
2022-06-03 21:35:19 +0630

    my ui added & all post data

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -5,6 +5,7 @@ import axios from "axios";
             return {
                 postLists: {},
                 categoryLists: {},
+                searchKey: '',
             }
         },
         methods: {
@@ -25,6 +26,40 @@ import axios from "axios";
             loadCategory () {
                 axios.get("http://127.0.0.1:8000/api/category").then((response) => {
                     this.categoryLists = response.data.category;
+                }).catch((error) => {
+                    console.log(error);
+                });
+            },
+            search(){
+                let search = {
+                    key: this.searchKey,
+                };
+                axios.post("http://127.0.0.1:8000/api/post/search",search).then((response) => {
+                    for(let i=0; i < response.data.post.length ; i++){
+                        if(response.data.post[i].image != null){
+                            response.data.post[i].image = "http://127.0.0.1:8000/postImage/"+response.data.post[i].image;
+                        }else{
+                            response.data.post[i].image = "http://127.0.0.1:8000/defaultImage/default_post.png";
+                        }
+                    }
+                    this.postLists = response.data.post;
+                }).catch((error) => {
+                    console.log(error);
+                });
+            },
+            searchCategory(id){
+                let search = {
+                    key: id,
+                };
+                axios.post("http://127.0.0.1:8000/api/category/search",search).then((response) => {
+                    for(let i=0; i < response.data.post.length ; i++){
+                        if(response.data.post[i].image != null){
+                            response.data.post[i].image = "http://127.0.0.1:8000/postImage/"+response.data.post[i].image;
+                        }else{
+                            response.data.post[i].image = "http://127.0.0.1:8000/defaultImage/default_post.png";
+                        }
+                    }
+                    this.postLists = response.data.post;
                 }).catch((error) => {
                     console.log(error);
                 });
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,109 +1,23 @@
 <template>
     
-    <section class="whats-news-area pt-50 pb-20">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="row d-flex justify-content-between align-items-center">
-                        <div class="col-lg-3 col-md-3">
-                            <div class="section-tittle mb-30">
-                                <h3>Whats New</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-9 col-md-9">
-                            <div class="properties__button">
-                                <!--Nav Button  -->
-                                <nav>
-                                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
-
-                                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="details.htmlnav-home" role="tab" aria-controls="nav-home" aria-selected="true">All</a>
-                                        <a class="nav-item nav-link " v-for="(category, index) in categoryLists" :key="index" id="nav-home-tab" data-toggle="tab" href="details.htmlnav-home" role="tab" aria-controls="nav-home" aria-selected="true">{{ category.title }}</a>
-                    
-                                    </div>
-                                </nav>
-                                <!--End Nav Button  -->
-                            </div>
-                        </div>
-                    </div>
+     <div class="row mt-5">
+        <!-- blog card  -->
+        <div class="col-6 col-md-12" v-for="(post , index) in postLists" :key="index">
+            <div class="card mb-4 d-flex shadow blog-card">
+                <div class="overflow-hidden img-container">
+                    <img v-bind:src="post.image" class="h-100" alt="" srcset="">
                 </div>
-            <div class="row">
-            <div class="col-12">
-                <!-- Nav Card -->
-                <div class="tab-content" id="nav-tabContent">
-                <!-- card one -->
-                <div
-                    class="tab-pane fade show active"
-                    id="nav-home"
-                    role="tabpanel"
-                    aria-labelledby="nav-home-tab"
-                >
-                    <div class="whats-news-caption">
-                    <div class="row">
-
-                        <div class="col-lg-6 col-md-6" v-for="(post , index) in postLists" :key="index">
-                            <div class="single-what-news mb-100">
-                                <div class="what-img">
-                                <img
-                                    v-bind:src="post.image"
-                                    alt=""
-                                />
-                                </div>
-                                <div class="what-cap">
-                                <span class="color1">Code Lab News</span>
-                                <h4>
-                                    <a href="details.html"
-                                    >{{ post.title }}</a
-                                    >
-                                </h4>
-                                </div>
-                            </div>
-                        </div>
-                        
-                    </div>
-                    </div>
+                <div class="card-body">
+                    <div class="badge py-2 rounded-0 shadow  bg-info">prgramming</div>
+                    <h5 class="text-white my-3">{{ post.title }}</h5>
+                    <p class="text-secondary">{{ post.description }}</p>
+                    <a href="" class="text-danger">Read More <i class="fas fa-arrow-right ms-3"></i></a>
                 </div>
-                
-                <!-- End Nav Card -->
-            </div>
-            </div>
                 </div>
-            </div>
-        </div>  
-             
-    </section>
-    <!--Start pagination -->
-        <div class="pagination-area pb-45 text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="single-wrap d-flex justify-content-center">
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination justify-content-start">
-                                    <li class="page-item">
-                                        <a class="page-link" href="details.html"><span class="flaticon-arrow roted"></span
-                      ></a>
-                                            </li>
-                                            <li class="page-item active">
-                                                <a class="page-link" href="details.html">01</a>
-                                            </li>
-                                            <li class="page-item">
-                                                <a class="page-link" href="details.html">02</a>
-                                            </li>
-                                            <li class="page-item">
-                                                <a class="page-link" href="details.html">03</a>
-                                            </li>
-                                            <li class="page-item">
-                                                <a class="page-link" href="details.html"><span class="flaticon-arrow right-arrow"></span
-                      ></a>
-                                            </li>
-                                            </ul>
-                                    </nav>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
         </div>
-                    <!-- End pagination  -->
+        <!-- blog card  -->
+        
+    </div>
     
 </template>
 
